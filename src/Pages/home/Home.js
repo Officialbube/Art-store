618987391c19ce83de0c8f08eb4061001c936f37
officialbube <ebubenkereebube@gmail.com>
2023-07-22 19:17:21 +0100

    added the featured product and card show components

--- src/Pages/home/Home.js
+++ src/Pages/home/Home.js
@@ -1,85 +1,33 @@
 import React from 'react'
-import { image1, image2, image3 } from '../../assets/image'
+
+import FeaturedProducts from '../../Components/featuredProducts/featuredProducts'
+
+import CardShow from '../../Components/slider/cardShow'
+import Slider from '../../Components/slider/Slider'
+
 
 
 const Home = () => {
   return (
     <section>
-      <h1 className='text-6xl font-medium text-center mx-36'>
+      <h1 className='text-3xl font-medium text-center mx-8'>
       Photography is poetry & beautiful untold stories
       </h1>
-      <p className='text-center mx-64 text-2xl font-medium leading-10 mt-12'>
+      <p className='text-center mx-6 text-base font-medium leading-6 mt-12'>
       Flip through more than 10,000 vintage shots,
       old photograghs, historic images and captures seamlessly in one place.
       Register to get top access.
       </p>
-      
-      <section className='px-28 pt-40'>
-      <h2 className='text-5xl font-medium'>Featured products</h2>
-      <div>
-        <div className='flex justify-between mt-9 border-t-2 py-14'>
-          <div>
-            <img src={image3}/>
-          </div>
-          <div className='flex flex-col justify-between w-96'>
-            <h3 className='text-4xl font-medium'>The Boolean Egyptian</h3>
-            <p>Lorem ipsum dolor sit amet, 
-              consectetur adipiscing elit ut aliquam, 
-              purus sit amet luctus venenatis, 
-              lectus magna fringilla urna, 
-              porttitor rhoncus dolor pur</p>
-            <div>
-              <div>
-                <p>64 major creators</p>
-              </div>
-              <div></div>
-            </div>
-          </div>
-        </div>
-        <div className='flex flex-row-reverse justify-between border-t-2 py-14'>
-          <div>
-          <img src={image2}/>
-          </div>
-          <div className='flex flex-col justify-between w-96'>
-          <h3 className='text-4xl font-medium'>The Boolean Egyptian</h3>
-            <p>Lorem ipsum dolor sit amet, 
-              consectetur adipiscing elit ut aliquam, 
-              purus sit amet luctus venenatis, 
-              lectus magna fringilla urna, 
-              porttitor rhoncus dolor pur</p>
-            <div>
-              <div>
-              <p>64 major creators</p>
-              </div>
-              <div></div>
-            </div>
-          </div>
-        </div>
-        <div className='flex  justify-between border-t-2 py-14'>
-          <div>
-          <img src={image1} className=''/>
-          </div>
-          <div className='flex flex-col justify-between w-96'>
-          <h3 className='text-4xl font-medium'>The Boolean Egyptian</h3>
-            <p>Lorem ipsum dolor sit amet, 
-              consectetur adipiscing elit ut aliquam, 
-              purus sit amet luctus venenatis, 
-              lectus magna fringilla urna, 
-              porttitor rhoncus dolor pur</p>
-            <div>
-              <div>
-              <p>64 major creators</p>
-              </div>
-              <div></div>
-            </div>
-          </div>
-        </div>
+      <div className='hidden sm:block'>
+      <Slider />
       </div>
-
-      </section>
-      <section>
-        <div className='h-96'></div>
-      </section>
+      <div className='h-80 w-96 mx-2.5 relative mt-7 sm:hidden'>
+        <CardShow/>
+      </div>
+      <FeaturedProducts />
+      
+      
+      
     </section>
   )
 }
