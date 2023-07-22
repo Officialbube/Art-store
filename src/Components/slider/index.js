b383aabeb05bfb863364afdbb876c556566eb2bd
officialbube <ebubenkereebube@gmail.com>
2023-07-22 19:19:33 +0100

    imported slides images

---
+++ src/Components/slider/index.js
@@ -0,0 +1,14 @@
+import slide1 from '../../assets/images/slide/Rectangle 230.png'
+import slide2  from '../../assets/images/slide/Rectangle 231.png'
+import slide3  from '../../assets/images/slide/Rectangle 232.png'
+import slide4  from '../../assets/images/slide/Rectangle 233.png'
+import slide5  from '../../assets/images/slide/Rectangle 234.png'
+
+export {default as fProd1} from '../../assets/images/featuredProducts/Rectangle 241.png'
+export {default as fProd2} from '../../assets/images/featuredProducts/Rectangle 230.png'
+export {default as fProd3} from '../../assets/images/featuredProducts/Rectangle 230 (1).png'
+
+
+
+
+export default [slide1,slide2,slide3,slide4,slide5];
