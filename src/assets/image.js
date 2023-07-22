341d4a1386d343d2bedc9549642dbae15a222523
officialbube <ebubenkereebube@gmail.com>
2023-07-22 19:13:23 +0100

    imported images

--- src/assets/image.js
+++ src/assets/image.js
@@ -1,9 +1,15 @@
-export { default as image1} from './images/Rectangle 230 (1).png'
-export { default as image2} from './images/Rectangle 230.png'
-export { default as image3} from './images/Rectangle 241.png'
-export { default as image4} from './images/Rectangle 242 (1).png'
-export { default as image5} from './images/Rectangle 244.png'
+import creator1 from './images/creators/creator1.png'
+import creator2 from './images/creators/creator2.png'
+import creator3 from './images/creators/creator3.png'
+import creator4 from './images/creators/creator4.png'
+import creator5 from './images/creators/creator5.png'
+
 export { default as image6} from './images/Rectangle 245.png'
-export { default as image7} from './images/Rectangle 246 (1).png'
 export { default as image8} from './images/Rectangle 246.png'
 export { default as image9} from './images/Rectangle 247.png'
+
+export {default as show1} from './images/cardShow/Rectangle 242 (1).png'
+export {default as show2} from './images/cardShow/Rectangle 244.png'
+export {default as show3} from './images/cardShow/Rectangle 246 (1).png'
+
+export const creators = [creator1,creator2,creator3,creator4,creator5];
