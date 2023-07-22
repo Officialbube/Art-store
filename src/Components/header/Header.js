b6264672c3a7b1a9cad51c9069ff0e375e6009e1
officialbube <ebubenkereebube@gmail.com>
2023-07-22 19:16:05 +0100

    added burger

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -8,21 +8,35 @@ import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNone
 const Header = () => {
   return (
     <div>
-      <nav className='flex justify-between items-center w-full px-44 h-24'>
-      <p className='text-4xl font-bold'>ARTSY.</p>
-      <nav className='flex items-center gap-x-12 text-2xl font-medium'>
-        <Link to="/">Home</Link>
-        <Link to="/market">Marketplace</Link>
-        <Link to="/auction">Auctions</Link>
-        <Link to="/drop">Drop</Link>
+      <nav className='flex justify-between items-center w-full px-4 h-24'>
+        <div className='flex flex-col gap-2 lg:hidden'>
+          <span className='bg-black w-6 h-px block'></span>
+          <span className='bg-black w-6 h-px block'></span>
+          <span className='bg-black w-6 h-px block'></span>
+        </div>
+        <p className='text-2xl font-bold'>ARTSY.</p>
+        <nav className='md:flex items-center gap-x-12 text-2xl font-medium hidden sm:hidden '>
+          <Link to="/">Home</Link>
+          <Link to="/market">Marketplace</Link>
+          <Link to="/auction">Auctions</Link>
+          <Link to="/drop">Drop</Link>
 
+        </nav>
+        <nav className='flex items-center gap-x-4'>
+          <a>
+            <SearchIcon />
+          </a>
+          <a className='' >
+            <ShoppingCartOutlinedIcon />
+          </a>
+          <a className='hidden lg:block'>
+            <NotificationsNoneOutlinedIcon  />
+          </a>
+
+
+
+        </nav>
       </nav>
-      <nav className='flex items-center gap-x-8'>
-        <SearchIcon />
-        <ShoppingCartOutlinedIcon />
-        <NotificationsNoneOutlinedIcon />
-      </nav>
-    </nav>
     </div>
   )
 }
