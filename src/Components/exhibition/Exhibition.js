4b6aac5d3b8966f2efdaaa5ba1957a983bc1945f
officialbube <ebubenkereebube@gmail.com>
2023-07-22 21:39:45 +0100

    created an exhibition component

---
+++ src/Components/exhibition/Exhibition.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Exhibition = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <div>
+                    <span></span>
+                    <p>Monalisa redefined in style.</p>
+                </div>
+                <div>
+                    <p></p>
+                    <div>
+                        <p>Start on : 08:00 GTS . Monday</p>
+                        <p>GET EXCLUSIVE VIEWING OF CONTEMPORARY ART AND CONNECT WITH INVESTORS AND AUCTIONEERS ACROSS THE WORLD BRINGING THEIR HIGHEST AND LOWEST BIDS.</p>
+                        <div>
+                            <a>See more</a>
+                            <button>Set a reminder</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Exhibition
