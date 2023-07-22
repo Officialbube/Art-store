e543849d88ac042190713d8f9d2a95ed4f207ef3
officialbube <ebubenkereebube@gmail.com>
2023-07-22 19:21:27 +0100

    added the card show images for mobile view

---
+++ src/Components/slider/cardShow.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { show1,show2,show3 } from '../../assets/image'
+
+const CardShow = () => {
+  return (
+    <div className='relative h-80'>
+        <img src={show1} className='absolute z-20 inset-0 m-auto'/>
+        <img src={show2} className='absolute inset-0 m-auto'/>
+        <img src={show3} className='absolute inset-0 m-auto'/>
+    </div>
+  )
+}
+
+export default CardShow
