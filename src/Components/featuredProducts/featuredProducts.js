6064534ec0ed1f6667c1511004fdfefc976566cd
officialbube <ebubenkereebube@gmail.com>
2023-07-22 19:18:59 +0100

    created the featured products component

---
+++ src/Components/featuredProducts/featuredProducts.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { featuredProducts } from './featuredProductData'
+import { BsArrowRight } from 'react-icons/bs'
+import { creators } from '../../assets/image'
+
+
+const FeaturedProducts = () => {
+ 
+    return (
+      <div className='mt-28 mb-8'>
+        <h2 className='text-3l font-bold px-3'>Featured Products</h2>
+            <div className=''>
+                {featuredProducts.map(slide => {
+                    return (
+                        <div key={slide.id} className='px-3 mb-8'>
+                          <div className='relative h-60'>
+                            <img src={slide.image} className='w-full absolute brightness-50 mb-4'/>
+                            <p className='text-3xl text-semibold absolute z-10 text-zinc-50 inset-x-0 m-auto top-12'>{slide.title}</p>
+                            <button className='absolute right-8 bottom-16 z-10 bg-transparent border-2 border-inherit h-14 w-14 text-3xl text-zinc-50 rounded-full flex justify-center items-center'>
+                              <BsArrowRight />
+                            </button>
+                          </div>
+                          <div>
+                            <p className='text-base font-normal leading-6'>{slide.desc}</p>
+                          </div>
+                          <div>
+                            {creators.map(creator => {
+                              return(
+                                <div>
+                                  <img src={creator}/>
+                                </div>
+                              )
+                            })}
+                          </div>
+                            
+                            
+                            
+                        </div>
+                        
+                    )
+                })}
+            </div>
+        </div>
+  )
+  
+}
+
+export default FeaturedProducts;
