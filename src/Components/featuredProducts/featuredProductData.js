5899a01d309f16c757eb31d3cf05e2e442d00de1
officialbube <ebubenkereebube@gmail.com>
2023-07-22 19:12:54 +0100

    created an array of objects for the featured products

---
+++ src/Components/featuredProducts/featuredProductData.js
@@ -0,0 +1,28 @@
+import { fProd1, fProd2, fProd3 } from "../slider/index"
+
+
+
+
+
+
+export const featuredProducts = [
+    {
+        id: 1,
+        image: fProd1,
+        title: 'Boolean Egyptian',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor pur',
+    },
+    {
+        id: 2,
+        image: fProd2,
+        title: 'Are we there yet?',
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor pur"
+    },
+    {
+        id: 3,
+        image: fProd3,
+        title: 'Oloibiri 1997',
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor pur"
+    },
+    
+]
