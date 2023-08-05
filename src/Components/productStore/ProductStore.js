f3e139e791bdc555324637325bf4b84741cf7166
officialbube <ebubenkereebube@gmail.com>
2023-08-05 07:51:35 +0100

    created product store

---
+++ src/Components/productStore/ProductStore.js
@@ -0,0 +1,41 @@
+import { prod1, prod2, prod3, prod4, prod5, prod6 } from "../../assets/image";
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        image: prod1,
+        name: 'PHILOMENA 22',
+        price: '$3.90'
+    },
+    {
+        id: 2,
+        image: prod2,
+        name: 'PHILOMENA 22',
+        price: '$3.90'
+    },
+    {
+        id: 3,
+        image: prod3,
+        name: 'PHILOMENA 22',
+        price: '$3.90'
+    },
+    {
+        id: 4,
+        image: prod4,
+        name: 'PHILOMENA 22',
+        price: '$3.90'
+    },
+    {
+        id: 5,
+        image: prod5,
+        name: 'PHILOMENA 22',
+        price: '$3.90'
+    },
+    {
+        id: 6,
+        image: prod6,
+        name: 'PHILOMENA 22',
+        price: '$3.90'
+    },
+]
+
