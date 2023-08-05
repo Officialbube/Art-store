df500b24c4c57096c62393c0c83a5032b600238e
officialbube <ebubenkereebube@gmail.com>
2023-08-05 07:47:22 +0100

    added images

--- src/assets/image.js
+++ src/assets/image.js
@@ -14,4 +14,11 @@ export {default as show3} from './images/cardShow/Rectangle 246 (1).png'
 
 export {default as galleryImg} from './images/gallery.jpeg'
 
+export {default as prod1} from './images/df459e1d97066ed90e131a19da154688.jpg'
+export {default as prod2} from './images/Rectangle 241.png'
+export {default as prod3} from './images/f345747082482f1fb22bc415f9748d6a.jpg'
+export {default as prod4} from './images/3371eec54d926c484dd69d7363432a27.jpg'
+export {default as prod5} from './images/c44f3d64696250f0b46f1f8b180d5abb.jpg'
+export {default as prod6} from './images/7306d8bca17164c353d00533e95d8c9c.jpg'
+
 export const creators = [creator1,creator2,creator3,creator4,creator5];
