2b59af2a8d7a766ce1b45506482001c1cd3c318a
officialbube <ebubenkereebube@gmail.com>
2023-08-05 07:56:10 +0100

    fix: responsiveness

--- src/Pages/home/Home.js
+++ src/Pages/home/Home.js
@@ -11,10 +11,10 @@ import Slider from '../../Components/slider/Slider'
 const Home = () => {
   return (
     <section className='w-screen overflow-hidden'>
-      <h1 className='text-3xl font-medium text-center mx-8'>
+      <h1 className='text-3xl lg:text-6xl  font-medium text-center mx-8 lg:px-28 lg:leading-[6rem]'>
         Photography is poetry & beautiful untold stories
       </h1>
-      <p className='text-center mx-6 text-base font-medium leading-6 mt-12'>
+      <p className='text-center mx-6 text-base font-medium leading-6 mt-12 lg:px-64 lg:text-2xl lg:leading-10'>
         Flip through more than 10,000 vintage shots,
         old photograghs, historic images and captures seamlessly in one place.
         Register to get top access.
