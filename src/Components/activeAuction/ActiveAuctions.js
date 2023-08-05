6d9f33ef8ab3984021f90cf016e18ef9b613c766
officialbube <ebubenkereebube@gmail.com>
2023-08-05 07:49:18 +0100

    created components for the active auctions

---
+++ src/Components/activeAuction/ActiveAuctions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ActiveAuctions = () => {
+  return (
+    <div>
+        <h3 className='text-xl lg:text-[1.75rem]  lg:mt-[3.75rem]'>Here’s an overview of products actively on auction, explore!</h3>
+    </div>
+  )
+}
+
+export default ActiveAuctions
--- src/Pages/auction/Auction.js
+++ src/Pages/auction/Auction.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import ActiveAuctions from '../../Components/activeAuction/ActiveAuctions'
 
 const Auction = () => {
   return (
-    <div>Auction</div>
+    <div className=' w-full px-2 lg:px-[7.5rem] overflow-x-hidden  lg:mt-8'>
+      <nav className='hidden lg:block text-lg font-medium lg:leading-[3.21rem] text-[#BCB7B7]'>
+      
+        <a>Home</a>/
+        <a className='text-[#292929]'>Auctions</a>
+      </nav>
+      <div>
+        <ActiveAuctions/>
+      </div>
+    </div>
   )
 }
 
