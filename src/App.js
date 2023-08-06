ee0960321bdf2e50ee3c992aadbc1e86e1cfccfc
officialbube <ebubenkereebube@gmail.com>
2023-08-06 12:39:31 +0100

    added route to product

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, RouterProvider, Routes, ScrollRestoration } from 'react-router-dom';
 import { Auction, Drop, Home, Market } from './Pages';
 import { Header, Footer } from './Components';
 import './App.css';
+import Product from './Components/product/Product';
 
 
 function App() {
   return (
+
     <BrowserRouter>
-      <Header/>
+
+      <Header />
+
       <Routes>
-        <Route path="/" element={ <Home/> }/>
-        <Route path="/market" element={ <Market/> }/>
-        <Route path="/auction" element={ <Auction/> }/>
-        <Route path="/drop" element={ <Drop/> }/>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/market" element={<Market />} />
+        <Route path="/auction" element={<Auction />} />
+        <Route path="/drop" element={<Drop />} />
+        <Route path="/market/:id" element={<Product />} />
       </Routes>
-    
+
     </BrowserRouter>
+
   );
 }
 
