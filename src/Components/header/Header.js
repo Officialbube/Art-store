782cd885e33c2752c7940a8fca10c08a9929bafe
officialbube <ebubenkereebube@gmail.com>
2023-08-07 18:59:57 +0100

    style the navigation for mobile view

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -3,12 +3,15 @@ import { Link } from 'react-router-dom'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
 
 
 const Header = () => {
   const navRef = useRef()
   const showNavBar = () => {
     navRef.current.classList.toggle('hidden')
+    navRef.current.classList.toggle('h-screen')
+    navRef.current.classList.toggle('w-screen')
   }
   return (
     <div className='relative bg-white z-[50] top-0'>
@@ -19,15 +22,25 @@ const Header = () => {
           <span className='bg-black w-6 h-px block'></span>
         </button>
         <Link to="/" className='text-2xl lg:text-4xl font-bold'>ARTSY.</Link>
-        <nav ref={navRef} className=' hidden flex flex-col items-center gap-x-12 gap-12 text-2xl font-medium absolute top-0 z-[200] sm:hidden h-screen w-screen  bg-white'>
-          <Link to="/" className=' z-[300] text-black'>Home</Link>
-          <Link to="/market">Marketplace</Link>
-          <Link to="/auction">Auctions</Link>
-          <Link to="/drop">Drop</Link>
-          <button onClick={showNavBar}>
-            <span className='bg-black w-6 h-px block'></span>
-            <span className='bg-black w-6 h-px block'></span>
-          </button>
+        <nav ref={navRef} className='hidden sm:flex flex flex-col lg:flex-row  lg:gap-x-12 gap-[1.12rem] px-[1.88rem] pt-6 overflow-hidden text-2xl font-medium absolute lg:relative top-0 left-0 z-[400]  bg-white'>
+          <div className='lg:hidden flex justify-between items-center  mb-[3.12rem]'>
+            <p className='text-2xl font-bold leading-[3.21rem] text-[#292929]'>Artsy.</p>
+            <button onClick={showNavBar} className='lg:hidden flex flex-col'>
+              <span className='bg-[#333333] w-[1.62rem] h-px block rotate-45 translate-y-px'></span>
+              <span className='bg-[#333333] w-[1.62rem] h-px block -rotate-45'></span>
+            </button>
+          </div>
+          <Link to="/" className=' z-[300] text-2xl font-medium leading-[3.21rem] text-[#333333]'>Home</Link>
+          <Link to="/market" className='text-2xl font-medium leading-[3.21rem] text-[#333333]'>Marketplace</Link>
+          <Link to="/auction" className='text-2xl font-medium leading-[3.21rem] text-[#333333]'>Auctions</Link>
+          <Link to="/drop" className='text-2xl font-medium leading-[3.21rem] text-[#333333]'>Drop</Link>
+
+          <div className='lg:hidden flex justify-end absolute bottom-16 right-8'>
+            <a className='lg:hidden w-16 h-16 rounded-full bg-[#3341c1] text-white grid place-items-center '>
+              <ChatBubbleIcon />
+            </a>
+          </div>
+
         </nav>
         <nav className='flex items-center gap-x-4'>
           <a>
@@ -37,7 +50,7 @@ const Header = () => {
             <ShoppingCartOutlinedIcon />
           </a>
           <a className='hidden lg:block'>
-            <NotificationsNoneOutlinedIcon  />
+            <NotificationsNoneOutlinedIcon />
           </a>
 
 
