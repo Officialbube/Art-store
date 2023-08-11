0095545dffb5356416332bcdaa924fb8eaed9bb4
officialbube <ebubenkereebube@gmail.com>
2023-08-11 23:09:18 +0100

    fix: changed the data type of the price from string to number to allow arithematic calculation

--- src/Components/productStore/ProductStore.js
+++ src/Components/productStore/ProductStore.js
@@ -5,37 +5,37 @@ export const PRODUCTS = [
         id: 1,
         image: prod1,
         name: 'PHILOMENA 22',
-        price: '$3.90'
+        price: 3.90
     },
     {
         id: 2,
         image: prod2,
         name: 'PHILOMENA 22',
-        price: '$3.90'
+        price: 3.90
     },
     {
         id: 3,
         image: prod3,
         name: 'PHILOMENA 22',
-        price: '$3.90'
+        price: 3.90
     },
     {
         id: 4,
         image: prod4,
         name: 'PHILOMENA 22',
-        price: '$3.90'
+        price: 3.90
     },
     {
         id: 5,
         image: prod5,
         name: 'PHILOMENA 22',
-        price: '$3.90'
+        price: 3.90
     },
     {
         id: 6,
         image: prod6,
         name: 'PHILOMENA 22',
-        price: '$3.90'
+        price: 3.90
     },
 ]
 
