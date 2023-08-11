7723d2a65ec29bd9aefa4cf1d0fa47fd55f96f2d
officialbube <ebubenkereebube@gmail.com>
2023-08-12 00:27:55 +0100

    created the cart page

--- src/Pages/cart/Cart.js
+++ src/Pages/cart/Cart.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import CartItem from '../../Components/cartItem/CartItem';
+
 const Cart = () => {
+  
   return (
-    <div>Cart</div>
+    <div>
+      <nav className=' text-base font-medium leading-[3.21rem] text-[#BCB7B7] px-[0.5rem]'>
+        <a>Home</a>/
+        <a>Marketplace</a>/
+        <a>Editorials</a>/
+        <a className='text-[#292929]'>Cart</a>
+        <hr />
+      </nav>
+      <div>
+        <div>
+          <CartItem/>
+        </div>
+      </div>
+      <div>
+      </div>
+    </div>
   )
 }
 
