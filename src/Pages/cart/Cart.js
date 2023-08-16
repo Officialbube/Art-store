6a441636045e8e3c1fe6c1d2f7d316acec4c2760
officialbube <ebubenkereebube@gmail.com>
2023-08-16 23:55:56 +0100

    styled the page

--- src/Pages/cart/Cart.js
+++ src/Pages/cart/Cart.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 import CartItem from '../../Components/cartItem/CartItem';
 
+
 const Cart = () => {
-  
+
   return (
-    <div>
-      <nav className=' text-base font-medium leading-[3.21rem] text-[#BCB7B7] px-[0.5rem]'>
+    <div className='px-[6.25rem]'>
+      <nav className=' text-base font-medium leading-[3.21rem] text-[#BCB7B7] px-[0.5rem] lg:hidden'>
         <a>Home</a>/
         <a>Marketplace</a>/
         <a>Editorials</a>/
@@ -15,10 +16,22 @@ const Cart = () => {
       </nav>
       <div>
         <div>
-          <CartItem/>
+          <div></div>
+          <div>
+            <CartItem />
+          </div>
         </div>
-      </div>
-      <div>
+        <div>
+          <div className='w-1/2'>
+            <button className='block bg-[#292929] text-white text-[2rem] font-medium py-8 px-[7.31rem]'>Proceed to checkout</button>
+            <a className='text-[1.75rem] font-medium text-[#272727] leading-[2.76rem] mt-10 underline'>continue shopping</a>
+          </div>
+          <div className='w-1/2'>
+            <p>Products in cart : <span></span></p>
+            <p>Shipping : <span>$2.50</span></p>
+            <p>Total : <span></span></p>
+          </div>
+        </div>
       </div>
     </div>
   )
